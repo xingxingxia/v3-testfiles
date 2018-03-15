e805d13e82b0886143d914e47c297a2b55267349
Xingxing Xia <xxia@redhat.com>
2018-03-15 15:15:36 +0800

    customize test http VS https

--- extensions/application-launcher.js
+++ extensions/application-launcher.js
@@ -1,11 +1,11 @@
 window.OPENSHIFT_CONSTANTS.APP_LAUNCHER_NAVIGATION = [{
   title: "Dashboard",                    // The text label
   iconClass: "fa fa-dashboard",          // The icon you want to appear
-  href: "http://example.com/dashboard",  // Where to go when this item is clicked
+  href: "http://www.bbc.com",  // Where to go when this item is clicked
   tooltip: 'View dashboard'              // Optional tooltip to display on hover
 }, {
   title: "Manage Account",
   iconClass: "pficon pficon-user",
-  href: "http://example.com/account",
+  href: "http://www.bbc.com/news",
   tooltip: "Update email address or password."
 }];
