dd4f2be32467176ae4c3ad1dfadfbba16483c068
Xingxing Xia <xxia@redhat.com>
2018-06-05 17:11:59 +0800

    add membership-whitelist.js

---
+++ extensions/membership-whitelist.js
@@ -0,0 +1,12 @@
+
+MEMBERSHIP_WHITELIST: [
+  "admin",
+  "basic-user",
+  "edit",
+  "system:deployer",
+  "system:image-builder",
+  "system:image-puller",
+  "system:image-pusher",
+  "view",
+  "custom-role-1"
+],
